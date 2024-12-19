bb4871c586b58cf2fc6c3465b36ba9e894ffa62c
ryan100705 <liryan704@gmail.com>
2024-12-19 14:55:52 -0500

    haram

---
+++ recipeMind/src/App.tsx
@@ -0,0 +1,6 @@
+import Button from "../src/components/Button";
+
+function App(){
+  return <div><Button/></div>
+}
+export default App;
---
+++ recipeMind/src/components/Button.tsx
@@ -0,0 +1,5 @@
+function Button() {
+    return <button>I am a button</button>
+}
+
+export default Button;
