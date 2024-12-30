7601ff0bae26530b12ceae6d6cf0cf55acbf07ae
ryan100705 <liryan704@gmail.com>
2024-12-30 17:40:28 -0500

    finish nav

--- recipeMind/src/App.tsx
+++ recipeMind/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Button from "./components/Button";
 import Nav from './components/nav/Nav';
+import Home from './pages/Home';
+import Map from './pages/Map';
+import SignIn from './pages/SignIn';
+import {Link} from "react-router-dom";
 
-function App(){
+
+const App = () => {
   return (
-  <div>
-    <Nav />
-    <Button/>
+    <BrowserRouter>
+    <div>
+      <Nav />
+      <Button/>
     </div>
+    <Routes>
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Map" element={<Map />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App;
